c34bd99d25d55b958e3117b89b82c0032abda136
villaswami <88075285+villaswami@users.noreply.github.com>
2024-08-03 10:09:39 +0530

    Add files via upload

---
+++ WeatherApp/Controllers/WeatherController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherApp.Models;
+
+namespace WeatherApp.Controllers
+{
+    public class WeatherController : Controller
+    {
+        private List<CityWeather> cities = new List<CityWeather>() {
+    new CityWeather() { CityUniqueCode = "LDN", CityName = "London", DateAndTime = Convert.ToDateTime("2030-01-01 8:00"), TemperatureFahrenheit = 33 },
+
+    new CityWeather() { CityUniqueCode = "NYC", CityName = "New York", DateAndTime = Convert.ToDateTime("2030-01-01 3:00"), TemperatureFahrenheit = 60 },
+
+    new CityWeather() { CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = Convert.ToDateTime("2030-01-01 9:00"), TemperatureFahrenheit = 82 }
+   };
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View(cities);
+        }
+        [Route("weather/{cityCode?}")]
+        public IActionResult City(string? cityCode)
+        {
+           
+            if (string.IsNullOrEmpty(cityCode))
+            {
+                return View();
+            }
+            CityWeather? city = cities.Where(temp => temp.CityUniqueCode == cityCode).FirstOrDefault();
+            return View(city);
+        }
+    }
+}
